5f6bd9b8dd26d069ed81a627b851a0bbba4b87c9
naiaragabriela <naigabi31@gmail.com>
2023-03-07 19:45:14 -0300

    comparacao string

---
+++ src/ProgramaComparacaoStrings/ProgramaComparacaoStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Digite a primeira palavra para comparação");
+string palavra1= Console.ReadLine();
+
+Console.WriteLine("Digite a segunda palavra para comparação");
+string palavra2= Console.ReadLine();
+
+void ComparacaoString()
+{
+    if (palavra1 != palavra2)
+    {
+        
+        Console.WriteLine("As palavras não são iguais");
+    }
+    else
+    { 
+
+        Console.WriteLine("As palavras são iguais");
+    }
+}
+ComparacaoString();
